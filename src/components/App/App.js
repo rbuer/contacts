cb1e55736bc22435e055bc6ccda9df2af26316c5
tuxfy <krueg.ja@web.de>
2019-12-05 19:52:58 +0100

    refactored: App with useState

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,44 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Ldap from '../../util/Ldap';
 import GlobalSearch from '../GlobalSearch/GlobalSearch';
 import ContactsList from '../ContactsList/ContactsList';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            contacts: [],
-            sortedBy: undefined,
-            sortDirection: 'asc'
-        };
-        this.getContacts = this.getContacts.bind(this);
-        this.sortData = this.sortData.bind(this);
-        this.searchData = this.searchData.bind(this);
-    }
 
-    getContacts() {
-        this.setState({ contacts: Ldap.getContacts() });
-    }
+const App = () => {
+    const [contacts, setContacts] = useState([]);
+    const [sortedBy, setSortedBy] = useState("");
+    const [sortDirection, setSortDirection] = useState("asc");
 
-    searchData(searchString){
-        this.setState({ contacts: Ldap.getContacts() }, function(){
-            const { contacts } = this.state;
-            const searchData = [];
-    
-            contacts.forEach(contact => {            
-                if(JSON.stringify(contact).includes(searchString)){
-                    searchData.push(contact);
-                };            
-            })   
-            this.setState({contacts: searchData});  
-        });             
+    // getContacts() {
+    //     this.setState({ contacts: Ldap.getContacts() });
+    // }
+
+    const searchData = (searchString) => {
+        const ldapConntacts = (Ldap.getContacts());
+        const filteredContacts = [];
+        ldapConntacts.forEach(contact => {            
+            if(JSON.stringify(contact).includes(searchString)){
+                filteredContacts.push(contact);
+            };            
+        })   
+        setContacts(filteredContacts); 
     }   
 
-    sortData(field, nextSortDirection) {
-        const { contacts } = this.state;
-
+    const sortData = (field, nextSortDirection)  => {
         let newData = [...contacts];
 
         const key = x => x[field];
@@ -50,34 +38,30 @@ class App extends Component {
             return reverse * ((a > b) - (b > a));
         });
 
-        this.setState({
-            contacts: sortedData,
-            sortedBy: field,
-            sortDirection: nextSortDirection
-        });
+        setContacts(sortedData);
+        setSortedBy(field);
+        setSortDirection(nextSortDirection);
     }      
     
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to
-                    reload.
-                </p>
-                <GlobalSearch onSearch={this.searchData} />
-                <ContactsList
-                    contacts={this.state.contacts}
-                    onSort={this.sortData}
-                    sortedBy={this.state.sortedBy}
-                    sortDirection={this.state.sortDirection}
-                />
+    return (
+        <div className="App">
+            <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>Welcome to React</h2>
             </div>
-        );
-    }
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to
+                reload.
+            </p>
+            <GlobalSearch onSearch={searchData} />
+            <ContactsList
+                contacts={contacts}
+                onSort={sortData}
+                sortedBy={sortedBy}
+                sortDirection={sortDirection}
+            />
+        </div>
+    );
 }
 
 export default App;
