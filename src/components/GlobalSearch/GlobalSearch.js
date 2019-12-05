455200f43d07e2c473533885da3970800c286f96
tuxfy <krueg.ja@web.de>
2019-12-05 20:17:54 +0100

    refactored: GlobalSearch to function

--- src/components/GlobalSearch/GlobalSearch.js
+++ src/components/GlobalSearch/GlobalSearch.js
@@ -1,48 +1,35 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropType from 'prop-types';
 import { Input } from 'react-rainbow-components';
 
-class GlobalSearch extends Component {
+const GlobalSearch = (props) => {
+    const searchInput=useRef(null);;
 
-    constructor(props){
-        super(props);
-        this.searchInput=React.createRef();
-        this.handleSearch=this.handleSearch.bind(this);
-        this.handleSearchKeyUp=this.handleSearchKeyUp.bind(this);
-    }
+    useEffect(() => {
+        searchInput.current.focus();   
+    });
 
-    componentDidMount(){
-        this.searchInput.current.focus();
-    }
-
-    handleSearch(e){
-        this.props.onSearch(e.target.value);
-    }
-
-    handleSearchKeyUp(e){
+    const handleSearchKeyUp = (e) => {
         if(e.key==='Enter'){
-            this.handleSearch(e);
+            props.onSearch(e.target.value);
         }
     }
 
-    render() {
-        return (
-                <Input
-                    label="Input Search"
-                    placeholder="Search"
-                    type="search"
-                    className="rainbow-p-around_medium"
-                    id='globalSearch'
-                    ref={this.searchInput}
-                    onKeyDown={this.handleSearchKeyUp}
-                />
-        );
-    }
+    return (
+            <Input
+                label="Input Search"
+                placeholder="Search"
+                type="search"
+                className="rainbow-p-around_medium"
+                id='globalSearch'
+                ref={searchInput}
+                onKeyDown={handleSearchKeyUp}
+            />
+    );
 }
 
 GlobalSearch.propType = {
     onSearch: PropType.func.isRequired,
 }
-
 
 export default GlobalSearch;
